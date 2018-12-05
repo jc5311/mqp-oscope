67241ade1484b942a3f9b5caa509053b20fc066e
JC5311 <jc.psp5@gmail.com>
2018-12-05 06:23:48 -0500

    Created a very basic (and ugly I'd add) gui app for the sake of seeing the general structure and syntax of a tkinter app. It let's you enter anything into a text field and creates a textbox after to show your text.:

--- guiTest.py
+++ guiTest.py
@@ -2,4 +2,32 @@ import Tkinter as tk
 
 window = tk.Tk() #create TK object as window
 
+window.title("RasPi_Scope")
+
+#funcs
+def say_something():
+	name = str(entry_field1.get())
+
+	#create text field
+	phrase_display = tk.Text(master=window, height=10, width=30)
+	#position it
+	phrase_display.grid(row=3, column=2)
+	phrase_display.insert(tk.END, name)
+
+#window properties
+width = window.winfo_screenwidth()
+height = window.winfo_screenheight()
+window.geometry(str(width) + 'x' + str(height)) #set window size to dims of screen
+#window.geometry("400x400")
+#label
+title = tk.Label(text="Enter your name", font=("Times New Roman", 20))
+title.grid(row=0, column=0)
+#entry field
+entry_field1 = tk.Entry()
+entry_field1.grid(row=2, column=1)
+#button
+button1 = tk.Button(text="Click me!", font=("Arial", 20), bg="red", command = say_something)
+button1.grid(row=1, column=1)
+
+#show screen
 window.mainloop() #run the window?
